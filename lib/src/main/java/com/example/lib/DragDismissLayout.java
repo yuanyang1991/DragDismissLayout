7c0b54940e635e376e7e6e383216107804d3a45a
yuanyang <yuanyang@douyu.tv>
2018-01-26 15:18:55 +0800

    增加缩放动画

--- lib/src/main/java/com/example/lib/DragDismissLayout.java
+++ lib/src/main/java/com/example/lib/DragDismissLayout.java
@@ -4,7 +4,6 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
@@ -13,7 +12,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
@@ -33,9 +31,20 @@ import android.widget.Scroller;
 
 public class DragDismissLayout extends FrameLayout {
 
-    private float dragRatio = 0.15f;//关闭动画开始的下拉屏幕比率
+    /**
+     * 透明度最大值
+     */
+    private static final int ALPHA = 255;
 
-    private float resistance = 0.6f;//阻力值
+    /**
+     * //关闭动画开始的下拉屏幕比率
+     */
+    private float dragRatio = 0.15f;
+
+    /**
+     * //滑动阻力值
+     */
+    private float resistance = 0.7f;
 
     /**
      * 动画时长
@@ -78,9 +87,14 @@ public class DragDismissLayout extends FrameLayout {
     private ColorDrawable colorDrawable;
 
     /**
-     * 上次滑动纵坐标，辅助滑动动画
+     * 上次滑动纵坐标
      */
     private int lastY = 0;
+
+    /**
+     *上次滑动横坐标
+     */
+    private int lastX = 0;
 
     /**
      * 目标View的横坐标
@@ -98,7 +112,7 @@ public class DragDismissLayout extends FrameLayout {
     private int width;
 
     /**
-     * 目标View的高都
+     * 目标View的高度
      */
     private int height;
 
@@ -106,6 +120,11 @@ public class DragDismissLayout extends FrameLayout {
      * 事件拦截最小高度
      */
     private int touchSlop;
+
+    /**
+     * 屏幕高度一半，用于临界点判断
+     */
+    private int halfWindowHeight;
 
 
     public DragDismissLayout(@NonNull Context context) {
@@ -132,6 +151,7 @@ public class DragDismissLayout extends FrameLayout {
         scroller = new Scroller(getContext());
         colorDrawable = new ColorDrawable(Color.parseColor("#000000"));
         setBackgroundDrawable(colorDrawable);
+        halfWindowHeight = windowHeight/2;
     }
 
     @Override
@@ -141,11 +161,12 @@ public class DragDismissLayout extends FrameLayout {
                 actionDownY = (int) ev.getRawY();
                 actionDownX = (int) ev.getRawX();
                 lastY = 0;
+                lastX = 0;
                 return false;
             case MotionEvent.ACTION_MOVE:
                 int yDistance = (int) (ev.getRawY() - actionDownY);
                 int xDistance = (int) (ev.getRawX() - actionDownX);
-                if (Math.abs(yDistance) > Math.abs(xDistance) && Math.abs(yDistance)>touchSlop/2){
+                if (Math.abs(xDistance) >touchSlop|| Math.abs(yDistance)>touchSlop){
                     return true;//事件拦截判断
                 }
                 break;
@@ -158,34 +179,48 @@ public class DragDismissLayout extends FrameLayout {
         switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:
                 actionDownY = (int) event.getRawY();
+                actionDownY = (int) event.getRawX();
                 lastY = 0;
+                lastX = 0;
                 return true;
             case MotionEvent.ACTION_MOVE:
-                int currentY = (int) event.getRawY();
-                final int distance = currentY - actionDownY;
+                final int currentY = (int) event.getRawY();
+                final int currentX = (int) event.getRawX();
+                final int yDistance = currentY - actionDownY;
+                final int xDistance = currentX - actionDownX;
                 int currentTop = mContentView.getTop();
-                if (distance <0&&currentTop<=0){
+                if (yDistance <0&&currentTop<=0){
                     //顶点处不可向上滑动
                     return super.onTouchEvent(event);
                 }
-                mContentView.offsetTopAndBottom((int) (distance*resistance));//使用offsetTopAndBottom产生滑动
-                colorDrawable.setAlpha((int) (255*computeAlpha((int) (windowHeight*0.5),currentTop)));
+                mContentView.offsetLeftAndRight((int) (xDistance*resistance));
+                mContentView.offsetTopAndBottom((int) (yDistance*resistance));//使用offsetTopAndBottom产生滑动
+                doScale(currentTop);
+                colorDrawable.setAlpha(computeAlpha(currentTop));
                 actionDownY = currentY;
+                actionDownX = currentX;
                 break;
             case MotionEvent.ACTION_UP:
                 int top =  mContentView.getTop();
+                int left = mContentView.getLeft();
                 //到达临界值，开始执行关闭动画
-                if (top >=windowHeight* dragRatio){
+                if (top >= windowHeight* dragRatio){
                     dismiss();
                 }else {
                     //没有到达临界值，返回顶部
-                    scroller.startScroll(0, 0,0,-top,animDuration);
+                    scroller.startScroll(0, 0,-left,-top,animDuration);
                     invalidate();
                 }
                 break;
         }
         return super.onTouchEvent(event);
     }
+
+   private void doScale(int currentTop){
+       final float scale = 1.0f*(windowHeight-currentTop)/windowHeight;
+       mContentView.setScaleX(scale);
+       mContentView.setScaleY(scale);
+   }
 
     private void dismiss() {
         colorDrawable.setAlpha(0);
@@ -234,29 +269,72 @@ public class DragDismissLayout extends FrameLayout {
         super.computeScroll();
         if (scroller.computeScrollOffset()) {
             int currentY = scroller.getCurrY();
-            int dis = currentY - lastY;
-            mContentView.offsetTopAndBottom(dis);
+            int currentX = scroller.getCurrX();
+            int yDis = currentY - lastY;
+            int xDis = currentX - lastX;
+            mContentView.offsetTopAndBottom(yDis);
+            mContentView.offsetLeftAndRight(xDis);
+            final int top = mContentView.getTop();
+            doScale(top);
+            colorDrawable.setAlpha(computeAlpha(top));
             lastY = currentY;
-            colorDrawable.setAlpha((int) (255*computeAlpha((int) (windowHeight*0.5),mContentView.getTop())));
+            lastX = currentX;
             invalidate();
         }
     }
 
-    private float computeAlpha(int h,int s) {
+    /**
+     * 计算此时背景透明度
+     * @param currentTop
+     * @return
+     */
+    private int computeAlpha(int currentTop) {
         //防止top< 0导致透明度反转
-        if (s<0){
-            return 1.0f;
+        if (currentTop <= 0){
+            return ALPHA;
         }
-        if (s > h){
-            s = h;
+
+        if (currentTop > halfWindowHeight){
+            currentTop = halfWindowHeight;
         }
-        return 1.0f*(h-s)/h;
+        return (int) (ALPHA*1.0f*(halfWindowHeight-currentTop)/halfWindowHeight);
     }
 
+    /**
+     * 设置目标View数据
+     * @param x        目标View绝对横坐标
+     * @param y        目标View绝对纵坐标
+     * @param width    目标View宽度
+     * @param height   目标View高度
+     */
     public void setTargetData(int x, int y, int width, int height) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+    }
+
+    public float getDragRatio() {
+        return dragRatio;
+    }
+
+    public void setDragRatio(float dragRatio) {
+        this.dragRatio = dragRatio;
+    }
+
+    public float getResistance() {
+        return resistance;
+    }
+
+    public void setResistance(float resistance) {
+        this.resistance = resistance;
+    }
+
+    public int getAnimDuration() {
+        return animDuration;
+    }
+
+    public void setAnimDuration(int animDuration) {
+        this.animDuration = animDuration;
     }
 }
